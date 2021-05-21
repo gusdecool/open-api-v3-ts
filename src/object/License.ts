55d1d10204c4b603ff0a92f434d3eb7920d088ad
Budi Arsana <gusdecool@gmail.com>
2021-05-22 00:21:13 +0800

    Add License.ts

--- src/object/License.ts
+++ src/object/License.ts
@@ -1,3 +1,21 @@
+/**
+ * License information for the exposed API.
+ * @see https://spec.openapis.org/oas/v3.1.0#license-object
+ */
 export default interface License {
-    // TODO implement
+    /**
+     * REQUIRED. The license name used for the API.
+     */
+    name: string;
+
+    /**
+     * An SPDX license expression for the API. The identifier field is mutually exclusive of the url field.
+     */
+    identifier?: string;
+
+    /**
+     * A URL to the license used for the API. This MUST be in the form of a URL. The url field is mutually exclusive
+     * of the identifier field.
+     */
+    url?: string;
 }
