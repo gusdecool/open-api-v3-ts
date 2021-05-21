c69f12bae474101a38a4e53b46dc34ed49e0ed8a
Budi Arsana <gusdecool@gmail.com>
2021-05-22 00:19:00 +0800

    Add Contact.ts

--- src/object/Contact.ts
+++ src/object/Contact.ts
@@ -1,3 +1,20 @@
+/**
+ * Contact information for the exposed API.
+ * @see https://spec.openapis.org/oas/v3.1.0#contact-object
+ */
 export default interface Contact {
-    // TODO implement
+    /**
+     * The identifying name of the contact person/organization.
+     */
+    name?: string;
+
+    /**
+     * The URL pointing to the contact information. This MUST be in the form of a URL.
+     */
+    url?: string;
+
+    /**
+     * The email address of the contact person/organization. This MUST be in the form of an email address.
+     */
+    email?: string;
 }
