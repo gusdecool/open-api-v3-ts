035dcd52e941e182a62475f82c0362aeca512e87
Budi Arsana <gusdecool@gmail.com>
2021-05-22 00:52:45 +0800

    Add ExternalDocumentation.ts

--- src/object/ExternalDocumentation.ts
+++ src/object/ExternalDocumentation.ts
@@ -1,3 +1,15 @@
+/**
+ * Allows referencing an external resource for extended documentation.
+ * @see https://spec.openapis.org/oas/v3.1.0#external-documentation-object
+ */
 export default interface ExternalDocumentation {
-    // TODO to implement
+    /**
+     * A description of the target documentation. CommonMark syntax MAY be used for rich text representation.
+     */
+    description?: string;
+
+    /**
+     * REQUIRED. The URL for the target documentation. This MUST be in the form of a URL.
+     */
+    url: string;
 }
