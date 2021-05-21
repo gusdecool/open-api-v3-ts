f7bcc33f83b6eff63512f310d78f306009b56d62
Budi Arsana <gusdecool@gmail.com>
2021-05-22 00:26:58 +0800

    Add ServerVariable.ts

--- src/object/ServerVariable.ts
+++ src/object/ServerVariable.ts
@@ -1,3 +1,23 @@
+/**
+ * An object representing a Server Variable for server URL template substitution.
+ * @see https://spec.openapis.org/oas/v3.1.0#server-variable-object
+ */
 export default interface ServerVariable {
+    /**
+     * An enumeration of string values to be used if the substitution options are from a limited set.
+     * The array MUST NOT be empty.
+     */
+    enum?: string[];
 
+    /**
+     * REQUIRED. The default value to use for substitution, which SHALL be sent if an alternate value is not supplied.
+     * Note this behavior is different than the Schema Object’s treatment of default values, because in those cases
+     * parameter values are optional. If the enum is defined, the value MUST exist in the enum’s values.
+     */
+    default: string;
+
+    /**
+     * An optional description for the server variable. CommonMark syntax MAY be used for rich text representation.
+     */
+    description?: string;
 }
